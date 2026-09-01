88392c5795653c28a4efaf45bdcece750eab423a
febeena sherin <febifebeenasherin@gmail.com>
2026-09-01 17:33:15 +0530

    [UPDATE]

---
+++ fetch_purchase_order/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Fetch Purchase order",
+    'version': "19.0.1.0.0",
+    'license':"LGPL-3",
+    'author': "Cybrosys Techno Solutions",
+    'category': 'Attendance',
+    'summary': 'migrate purchase order from odoo16 to odoo19',
+    'description': """migrate purchase order""",
+    'website': 'http://www.cybrosys.com',
+    'sequence': 2,
+    'application': False,
+    'installable': True,
+    'auto_install': False,
+    'depends': ['base', 'purchase'],
+    'data':[
+
+        "/home/cybrosys/odoo-19/custom/fetch_purchase_order/views/purchase_order_menu.xml",
+     ],
+}
+
